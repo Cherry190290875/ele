82f5bd249cb72757961b200d83a52ecfacb802c6
[Cherry190290875] <[156828386@qq.com]>
2018-04-13 14:36:38 +0800

    ele project start

---
+++ src/components/header/header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="head-top">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return {
+           msg:'我是一直猪'
+        }
+      }
+    }
+</script>
+
+<style>
+  #head-top {
+    background-color: dodgerblue;
+    position: fixed;
+    left: 0;
+    top:0;
+    width: 100%;
+    height: 1.95rem;
+    color:#FFF;
+  }
+</style>
